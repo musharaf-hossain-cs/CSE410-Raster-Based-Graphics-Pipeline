6a3140635bf9e88aac8f4d711d7952337672865e
Md. Musharaf Hossain <mdmusharaf8071@gmail.com>
2022-07-20 00:23:22 +0600

    First Commit

---
+++ My Assignment/1705050.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Point{
+    double x, y, z;
+    Point(double x=0, double y=0, double z=0){
+        this->x = x;
+        this->y = y;
+        this->z = z;
+    }
+};
+
+int main(){
+    cout<<"Initiated Successfully!"<<endl;
+}
