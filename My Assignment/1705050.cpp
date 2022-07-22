9bd3bbb22419e0ae5c5f705721d19a08d4fc4cda
Md. Musharaf Hossain <mdmusharaf8071@gmail.com>
2022-07-22 14:32:30 +0600

    ouput.bmp is generated. zBuffer.txt is remaining

--- My Assignment/1705050.cpp
+++ My Assignment/1705050.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+#include "bitmap_image.hpp"
 using namespace std;
 
 #define dbg(x) cout<<#x<<": "<<x<<endl;
@@ -27,6 +28,80 @@ struct Point{
         dbg(z);
     }
 };
+
+struct Color{
+    int r,g,b;
+    Color(){
+        r=g=b=255;
+    }
+    Color(int R, int G, int B){
+        r = R;
+        g = G;
+        b = B;
+    }
+};
+
+struct Column{
+    double xs;
+    double zs;
+    Column(){
+        xs=0;
+        zs=0;
+    }
+    Column(double x, double z){
+        xs = x;
+        zs = z;
+    }
+};
+
+struct Triangle{
+    Point points[3];
+    Color color;
+
+    Triangle(){
+
+    }
+    Triangle(Point A, Point B, Point C){
+        points[0] = A;
+        points[1] = B;
+        points[2] = C;
+    }
+    void setPoint(Point A, Point B, Point C){
+        points[0] = A;
+        points[1] = B;
+        points[2] = C;
+    }
+    void setColor(int r, int g, int b){
+        color.r = r;
+        color.g = g;
+        color.b = b;
+    }
+
+    double getMinX(){
+        return min(min(points[0].x, points[1].x), points[2].x);
+    }
+
+    double getMaxX(){
+        return max(max(points[0].x, points[1].x), points[2].x);
+    }
+
+    double getMinY(){
+        return min(min(points[0].y, points[1].y), points[2].y);
+    }
+
+    double getMaxY(){
+        return max(max(points[0].y, points[1].y), points[2].y);
+    }
+
+    void Print(){
+        cout<<"Triangle:"<<endl;
+        cout<<"\tP1: "<<fixed<<setprecision(7)<<points[0].x<<' '<<points[0].y<<' '<<points[0].z<<endl;
+        cout<<"\tP2: "<<fixed<<setprecision(7)<<points[1].x<<' '<<points[1].y<<' '<<points[1].z<<endl;
+        cout<<"\tP3: "<<fixed<<setprecision(7)<<points[2].x<<' '<<points[2].y<<' '<<points[2].z<<endl;
+        cout<<"\tColor: rgb("<<color.r<<','<<color.g<<','<<color.b<<")"<<endl;
+    }
+};
+
 
 struct Matrix{
     double data[4][4];
@@ -66,6 +141,18 @@ struct Matrix{
         }
         file<<endl;
         return;
+    }
+
+    Triangle getTriangle(){
+        Point p[3];
+        for(int i=0;i<3;i++){
+            p[i].x = data[0][i] / data[3][i];
+            p[i].y = data[1][i] / data[3][i];
+            p[i].z = data[2][i] / data[3][i];
+        }
+
+        Triangle t(p[0], p[1], p[2]);
+        return t;
     }
 
     Matrix Transpose(){
@@ -109,6 +196,7 @@ Point eye, look, up, L, U, R;
 double fovY, aspectRatio, near, far;
 Matrix P, V;
 Matrix transformation;
+vector<Triangle> triangles;
 stack<Matrix> stk;
 
 void PopMatrix(){
@@ -282,6 +370,231 @@ void Rotate1(double angle, Point a){
     transformation = Multiply(transformation, rotation);
 }
 
+
+
+///*********************************Z-buffer********************************
+double leftLimit, rightLimit, topLimit, bottomLimit;
+double leftX, rightX;
+double topY, bottomY;
+double dx, dy;
+int scrHeight, scrWidth;
+double front, rear;
+
+// value of FindLeftRightScanline...
+double z1, z2;
+
+void inputConfig(){
+    ifstream in;
+    in.open("config.txt");
+    in >> scrWidth >> scrHeight;
+    in >> leftLimit;
+    in >> bottomLimit;
+    in >> front >> rear;
+
+    rightLimit = -leftLimit;
+    topLimit = -bottomLimit;
+}
+
+void setColors(){
+    int s = triangles.size();
+    int r,g,b;
+    for(int i=0; i<s; i++){
+        r = rand() % 256;
+        g = rand() % 256;
+        b = rand() % 256;
+        triangles[i].setColor(r,g,b);
+    }
+}
+
+void printTriangles(){
+    for(Triangle t: triangles){
+        t.Print();
+    }
+}
+
+vector<Column> GetColumn(Triangle t, double ys){
+    // Ref: given z-buffer calculation
+    vector<Column> columns;
+    Point a, b;
+    double x, z;
+    int s;
+    int flag1=-1, flag2=-1;
+    double minX = t.getMinX();
+    double maxX = t.getMaxX();
+
+    a = t.points[0];
+    b = t.points[1];
+    x = a.x + ((ys-a.y)/(b.y-a.y))*(b.x-a.x);
+    z = a.z + ((ys-a.y)/(b.y-a.y))*(b.z-a.z);
+    s = (int) round(abs(leftX - x)/dx);
+    if( x >= minX && x <= maxX){
+        flag1 = s;
+        columns.push_back(Column(x,z));
+    }
+
+    a = t.points[1];
+    b = t.points[2];
+    x = a.x + ((ys-a.y)/(b.y-a.y))*(b.x-a.x);
+    z = a.z + ((ys-a.y)/(b.y-a.y))*(b.z-a.z);
+    s = (int) round(abs(leftX - x)/dx);
+    if(x >= minX && x <= maxX && s != flag1){
+        columns.push_back(Column(x,z));
+        flag2 = s;
+    }
+
+    a = t.points[2];
+    b = t.points[0];
+    x = a.x + ((ys-a.y)/(b.y-a.y))*(b.x-a.x);
+    z = a.z + ((ys-a.y)/(b.y-a.y))*(b.z-a.z);
+    s = (int) round(abs(leftX - x)/dx);
+    if(x >= minX && x <= maxX && s != flag1 && s!= flag2){
+        columns.push_back(Column(x,z));
+    }
+
+    return columns;
+}
+
+void initiateZbuffer(){
+    dx = (rightLimit - leftLimit) / (double)scrWidth;
+    dy = (topLimit - bottomLimit) / (double)scrHeight;
+    topY = topLimit - dy/2;
+    bottomY = bottomLimit + dy/2;
+    leftX = leftLimit + dx/2;
+    rightX = rightLimit - dx/2;
+}
+
+pair<int,int> FindLeftRightScanline(Triangle t, double ys){
+    vector<Column> columns = GetColumn(t, ys);
+    double minX, maxX;
+    if(columns.empty()) return make_pair(-1,-1);
+    else if(columns.size() == 1){
+        minX = columns[0].xs;
+        maxX = columns[0].xs;
+        z1 = columns[0].zs;
+        z2 = columns[0].zs;
+    }
+    else if(columns[0].xs <= columns[1].xs){
+        minX = columns[0].xs;
+        maxX = columns[1].xs;
+        z1 = columns[0].zs;
+        z2 = columns[1].zs;
+    }
+    else{
+        minX = columns[1].xs;
+        maxX = columns[0].xs;
+        z1 = columns[1].zs;
+        z2 = columns[0].zs;
+    }
+    int left, right;
+
+    if(minX < leftX){
+        left = 0;
+    }
+    else {
+        left = (int) round(abs(leftX - minX)/dx);
+    }
+
+    if(maxX > rightX){
+        right = scrWidth - 1;
+    }
+    else{
+        right = (int) round(abs(leftX-maxX)/dx);
+    }
+
+    return make_pair(left,right);
+}
+
+pair<int,int> FindTopBottomScanline(Triangle t){
+    double maxY = t.getMaxY();
+    double minY = t.getMinY();
+    int topScanline, bottomScanline;
+
+    if(maxY > topY){
+        topScanline = 0;
+    }
+    else{
+        topScanline = (int) round(abs(topY - maxY)/dy);
+    }
+
+    if(minY < bottomY){
+        bottomScanline = scrHeight - 1;
+    }
+    else{
+        bottomScanline = (int) round(abs(topY-minY)/dy);
+    }
+    return make_pair(topScanline, bottomScanline);
+}
+
+void zBufferAlgo(){
+    vector<vector<double>> zBuffer;
+    bitmap_image image(scrWidth, scrHeight);
+
+    // background color
+    for(int i=0; i<scrHeight; i++){
+        vector<double> row;
+        for(int j=0; j<scrWidth; j++){
+            row.push_back(rear);
+            image.set_pixel(i,j,0,0,0);
+        }
+        zBuffer.push_back(row);
+    }
+
+    /// Spec Pseudocode
+
+    // for each object : Triangles
+    for(int i=0; i<triangles.size(); i++){
+        // Find top_scanline and bottom_scanline after necessary clipping
+        Triangle t = triangles[i];
+        double minX = t.getMinX();
+        double maxX = t.getMaxX();
+        pair<int,int> topBottom = FindTopBottomScanline(t);
+        int topScanline = topBottom.first;
+        int bottomScanline = topBottom.second;
+
+        // for row_no from top_scanline to bottom_scanline
+        for(int row = topScanline; row <= bottomScanline; row++){
+            // Find left_intersecting_column and right_intersecting_column
+            // after necessary clipping
+            double ys = topY - row*dy;
+            pair<int,int> leftRight = FindLeftRightScanline(t,ys);
+            if(leftRight.first < 0) continue;
+            int leftScanline = leftRight.first;
+            int rightScanline = leftRight.second;
+
+            double increment = (z2-z1)/(rightScanline-leftScanline);
+            if(increment < 0) increment = 0;
+
+            if(maxX - minX > 0){
+                // for col_no from left_intersecting_column to right_intersecting_column
+                double z=z1;
+                for(int col=leftScanline; col <= rightScanline; col++, z+=increment){
+                    // z < zBuffer[row][col] --> this checks for multiple object. nearest one chosen
+                    // Compare with Z-buffer and z_front_limit and update if required
+                    if(z >= front && z <= rear && z < zBuffer[row][col]){
+                        // z values is calculated in GetColumn function
+                        zBuffer[row][col] = z;
+                        // Update pixel information if required
+                        image.set_pixel(col, row, t.color.r, t.color.g, t.color.b);
+                    }
+                }
+            }
+        }
+    }
+
+    image.save_image("output.bmp");
+}
+
+void zBufferUtil(){
+    inputConfig();
+    initiateZbuffer();
+    setColors();
+    zBufferAlgo();
+
+//    printTriangles();
+}
+
+
+
 void Initiate(){
     // calculate L
     L.x = look.x - eye.x;
@@ -339,6 +652,7 @@ void Initiate(){
 int main(){
     ifstream scene;
     ofstream stage1, stage2, stage3;
+    srand(time(0));
 
     scene.open("scene.txt");
     stage1.open("stage1.txt");
@@ -376,6 +690,7 @@ int main(){
 
             // stage 2
             triangle = Multiply(P, triangle);
+            triangles.push_back(triangle.getTriangle());
             triangle.Print(stage3);
         }
         else if(command == "translate"){
@@ -405,5 +720,9 @@ int main(){
             break;
         }
     }
+    stage1.close();
+    stage2.close();
+    stage3.close();
 
+    zBufferUtil();
 }
